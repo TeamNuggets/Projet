25b8692b45f5328ef3fd105a47b70c3987a9a975
MateoAlexandre <57395513+MateoAlexandre@users.noreply.github.com>
2019-12-03 11:00:05 +0100

    Add files via upload
    fonction pour les décimaux

---
+++ ordonner4.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void ordonnerTableau(double tableau[], int tailleTableau);
+void afficheNouveauTableau(double tableau[], int tailleTableau);
+ 
+int main(int argc, char *argv[])
+{
+    double tableau[6] = {6.28,3.63,11.74,7.79,8.15,7.36};
+    afficheNouveauTableau(tableau,6);
+    ordonnerTableau(tableau,6);
+    afficheNouveauTableau(tableau,6);
+    return 0;
+}
+void afficheNouveauTableau(double tableau[], int tailleTableau)
+{
+    int k;
+ 
+    for (k = 0 ; k < tailleTableau ; k++)
+    {
+        printf("%f", tableau[k]);
+    }
+    printf("\n");
+ 
+}
+void ordonnerTableau(double tableau[], int tailleTableau)
+{
+    int i,temp,erreur;
+    do
+    {
+        erreur=0;
+        for(i=0;i<tailleTableau-1;i++)
+            {
+                if(tableau[i]>tableau[i+1])
+                {
+                    erreur=1;
+                    temp=tableau[i];
+                    tableau[i]=tableau[i+1];
+                    tableau[i+1]=temp;
+                    afficheNouveauTableau(tableau,6);
+ 
+                }
+ 
+            }
+        }
+    while (erreur!=0);
+}
